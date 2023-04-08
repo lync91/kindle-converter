f788a233a7fd2ca430335e21974766fa59f0e0ea
lync91 <mr.lync91@gmail.com>
2023-04-08 14:06:07 +0700

    update

--- index.js
+++ index.js
@@ -1,78 +1,47 @@
 const cheerio = require('cheerio');
-var fs      = require('fs');
-var url     = require('url');
-var http    = require('http');
-var https   = require('https');
+var fs = require('fs');
+var wget = require('./wget64');
+var path = require('path')
+var xtend = require('xtend')
+var convert = require('ebook-convert')
 
-function download(href, encoding, onsuccess, onerror) {
-  onsuccess = (typeof onsuccess == 'function') ? onsuccess : function() {};
-  onerror   = (typeof onerror   == 'function') ? onerror   : function() {};
-
-  var protocol = url.parse(href).protocol;
-  var get = function() {
-    console.error(protocol + ' is not supported');
-  };
-
-  switch (protocol) {
-    case 'http:':
-      get = http.get;
-      break;
-    case 'https:':
-      get = https.get;
-      break;
-  }
-
-  get(href, function(res) {
-    var data = '';
-    if (encoding) {
-      res.setEncoding(encoding);
-      if (encoding == 'base64') {
-        data = 'data:' + mime.getType(href) + ';base64,';
-      }
-    }
-    res.on('data', function(chunk) { data += chunk; });
-    res.on('end', function() { onsuccess(data); });
-  }).on('error', function() { onerror(); });
+var options = {
+  input: path.join(__dirname, 'test.html'),
+  output: path.join(__dirname, 'example.epub'),
+  authors: '"Lync"',
+  pageBreaksBefore: '//h:h1',
+  chapter: '//h:h1',
+  insertBlankLine: true,
+  insertBlankLineSize: '1',
+  lineHeight: '12',
+  marginTop: '50',
+  marginRight: '50',
+  marginBottom: '50',
+  marginLeft: '50'
 }
-
-// download(inputURL, '', function(data) {
-//   var $ = cheerio.load(data);
-
-//   var baseHref = inputURL.replace(/[^\/]*$/, '');
-//   if ($('base').length) {
-//     // XXX does not work if <base> has a relative URL
-//     baseHref = $('base').last().attr('href');
-//   } else {
-//     $('head').prepend('\n  <base href="' + baseHref + '" />');
-//   }
-
-//   inlineStylesheets($, baseHref, function(doc) {
-//     inlineScripts($, baseHref, function(doc) {
-//       getMediaURLs($, baseHref, function(dataURLs) {
-//         useDataURLs($, baseHref, dataURLs);
-//         console.log('==> ' + outputFile);
-//         fs.writeFileSync(outputFile, $.html());
-//       });
-//     });
-//   });
-// }, function(error) {
-//   console.error('Something went wrong. Now guess what.');
-// });
+ 
+/*
+* create epub file
+*/
 
 
-download('https://rust-tieng-viet.github.io/print.html', '', function (data) {
+wget.wgetcb('https://rust-tieng-viet.github.io/print.html', function (data) {
   var $ = cheerio.load(data);
   const main = $('main');
-  
+
   fs.readFile('./tmp.html', 'utf-8', function (err, tmpdata) {
     const tmp = cheerio.load(tmpdata);
     tmp('body').html(main.html());
     fs.writeFile('./test.html', tmp.html(), 'utf8', function (er) {
-      console.log(er);
+      // console.log(er);
+      if (!er) {
+        convert(options, function (err) {
+          if (err) console.log(err)
+        })
+      }
     })
   })
 })
-
 
 
 
